0f8d0f013aabd5ebb4ebdc1a88d0ad3b7169eef7
KSPAtlas <kspatlas@disroot.org>
2024-11-02 17:56:30 +0000

    fix fastfetch more

--- hm/ksp.nix
+++ hm/ksp.nix
@@ -36,6 +36,37 @@
           type = "sixel";
           source = ../assets/nixlogo.png;
         };
+        modules = [
+          "title"
+          "separator"
+          "os"
+          "host"
+          "kernel"
+          "uptime"
+          "packages"
+          "shell"
+          # "display"
+          "de"
+          "wm"
+          "wmtheme"
+          "theme"
+          "icons"
+          "font"
+          "cursor"
+          "terminal"
+          "terminalfont"
+          "cpu"
+          "gpu"
+          "memory"
+          "swap"
+          "disk"
+          "localip"
+          "battery"
+          "poweradapter"
+          "locale"
+          "break"
+          "colors"
+        ];
       };
     };
   };
