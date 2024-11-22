f5c968ea89937da43419142d6fdae1d0564e483c
KSPAtlas <kspatlas@disroot.org>
2024-11-22 16:08:42 +0000

    disable open km

--- nixos/nvidia.nix
+++ nixos/nvidia.nix
@@ -34,7 +34,7 @@
     # https://github.com/NVIDIA/open-gpu-kernel-modules#compatible-gpus 
     # Only available from driver 515.43.04+
     # Currently "beta quality", so false is currently the recommended setting.
-    open = true;
+    open = false;
 
     # Enable the Nvidia settings menu,
     # accessible via `nvidia-settings`.
