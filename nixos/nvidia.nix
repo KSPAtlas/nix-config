4d81287790c512270e02b6733040fd6f89f94288
KSPAtlas <kspatlas@disroot.org>
2024-11-22 16:24:00 +0000

    reenable open, switch to stable

--- nixos/nvidia.nix
+++ nixos/nvidia.nix
@@ -34,14 +34,14 @@
     # https://github.com/NVIDIA/open-gpu-kernel-modules#compatible-gpus 
     # Only available from driver 515.43.04+
     # Currently "beta quality", so false is currently the recommended setting.
-    open = false;
+    open = true;
 
     # Enable the Nvidia settings menu,
     # accessible via `nvidia-settings`.
     nvidiaSettings = true;
 
     # Optionally, you may need to select the appropriate driver version for your specific GPU.
-    package = config.boot.kernelPackages.nvidiaPackages.beta;
+    package = config.boot.kernelPackages.nvidiaPackages.stable;
   };
 }
 
